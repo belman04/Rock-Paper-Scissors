433ee9f65b47ff12bbfa56d3abebe37f2f76f98b
Andrea Belman <a21490528@itmexicali.edu.mx>
2025-05-11 13:10:44 -0700

    Modified logic to be able to be displayed visually

--- main.py
+++ main.py
@@ -1,26 +1,47 @@
-# import sys
 import pygame # importing the pygame library
 import random # importing the random library
 
 def main():
     pygame.init() # calling funtion init to initialize pygame
 
-    window = pygame.display.set_mode((600, 600)) # creating the window 
+    window = pygame.display.set_mode((600, 500)) # creating the window 
     pygame.display.set_caption("Rock Paper Scissors") # setting the window title
-    pygame.display.set_icon(pygame.image.load("icon.jpg")) # setting icon 
+    pygame.display.set_icon(pygame.image.load("assets/icon.jpg")) # setting icon 
     
-    choices = ["rock", "paper", "scissors"] # list of choices
+    # setting fonts size and style
+    font_title = pygame.font.SysFont("Arial", 40,)
+    font_choices = pygame.font.SysFont("Arial", 30, italic=True)
+    font_result = pygame.font.SysFont("Arial", 35, bold=True)
+    
+    # rendering the text
+    text_title = font_title.render("Chose", True, "black", "green")    
+    text_rock = font_choices.render("Rock", True, "black", "blue")
+    text_paper = font_choices.render("Paper", True, "black", "blue")
+    text_scissors = font_choices.render("Scissors", True, "black", "blue")
+    text_result = font_result.render("", True, "black", "purple")
+    
+    # text position
+    title_rect = text_title.get_rect(center=(300, 50))
+    rock_rect = text_rock.get_rect(center=(130, 150))
+    paper_rect = text_paper.get_rect(center=(300, 150))
+    scissors_rect = text_scissors.get_rect(center=(470, 150))
+    result_rect = text_result.get_rect(center=(300, 250))
+    
+    def play_game(choice):
+        choices = ["rock", "paper", "scissors"] # list of choices
 
-    choice = input("Chose rock, paper or scissors: ") # asking the user for their choice in console
-
-    if choice in choices:
-        show_choice(choice)
+        nonlocal text_result, result_rect # to modify the variable 
+        
+        print("You chose ", choice)
+        
         computer_choice = random.choice(choices) # random choice for the computer
         print("Computer chose", computer_choice)
+        
         result = check_winner(choice, computer_choice)
         print(result)
-    else:
-        print("Invalid choice, try again")
+        
+        text_result = font_result.render(f"Computer: {computer_choice} - {result}", True, "black", "purple")
+        result_rect = text_result.get_rect(center=(300, 250))
 
     # closing the window if the user clicks the close button
     running = True
@@ -29,23 +50,26 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-
+            elif event.type == pygame.MOUSEBUTTONDOWN: # if the user click the mouse
+                if rock_rect.collidepoint(event.pos): 
+                    play_game("rock")
+                elif paper_rect.collidepoint(event.pos):
+                    play_game("paper")
+                elif scissors_rect.collidepoint(event.pos):
+                    play_game("scissors")
+                
         window.fill("white") # to clear the window
-        pygame.display.flip() # to update the window
         
+        # drawing the text
+        window.blit(text_title, title_rect) 
+        window.blit(text_rock, rock_rect) 
+        window.blit(text_paper, paper_rect) 
+        window.blit(text_scissors, scissors_rect)
+        window.blit(text_result, result_rect)
+        
+        pygame.display.flip() # to update the window
 
     pygame.quit() # calling function quit to quit pygame, always at the end
-    # sys.exit() # calling the exit funtion to exit the window, not neccesary
-
-# funtion just to show your choice in console
-def show_choice(choice):
-    match choice:
-        case "rock":
-            print("You chose rock")
-        case "paper":
-            print("You chose paper")
-        case "scissors":
-            print("You chose scissors")
 
 # function to compare choices and check winner in console
 def check_winner(your_choice, computer_choice):
